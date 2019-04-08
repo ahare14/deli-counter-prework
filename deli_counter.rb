d911672db9365ce135700b7f357ed1bde67977c8
Alexander Hare <alexanderhare14@gmail.com>
2019-04-08 23:05:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,4 +5,12 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currenlty empty."
+  else
+    message = "The line is currently:"
+    katz_deli.each_with_index do |i, val|
+      message += " #{val + 1}. #{i}"
+      puts message
+    end
+  end
+end
     
