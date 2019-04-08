92f43c5132de5f40509181688db7081ca59bcc46
Alexander Hare <alexanderhare14@gmail.com>
2019-04-08 23:06:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,36 @@ def line(katz_deli)
     message = "The line is currently:"
     katz_deli.each_with_index do |i, val|
       message += " #{val + 1}. #{i}"
-      puts message
     end
+    puts message
+  end
+end
+
+
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    message = "The line is currently:"
+    katz_deli.each_with_index do |i, val|
+      message += " #{val + 1}. #{i}"
+    end
+    puts message
+  end
+end
+
+def take_a_number(katz_deli,name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift()
   end
 end
